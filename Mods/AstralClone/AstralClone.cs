4b0e9e8506318b29a060d318b30c69683b6c0e34
Thune Matheo <thunem76@gmail.com>
2024-12-06 02:44:34 +0100

    AstralClone

---
+++ Mods/AstralClone/AstralClone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using MelonLoader;
+using VRC.Core;
+
+namespace Astrum
+{
+    public class AstralClone : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonLogger.Msg("Patching AllowAvatarCopying...");
+            base.HarmonyInstance.Patch(typeof(APIUser).GetProperty("allowAvatarCopying").GetSetMethod(), new HarmonyMethod(typeof(AstralClone).GetMethod("Hook", BindingFlags.Static | BindingFlags.NonPublic)), null, null, null, null);
+            MelonLogger.Msg("AllowAvatarCopying Patched !");
+        }
+
+        private static void Hook(ref bool __0)
+        {
+            __0 = true;
+        }
+    }
+}
---
+++ Mods/AstralClone/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using Astrum;
+using MelonLoader;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: MelonInfo(typeof(AstralClone), "AstralClone", "0.1.0", null, "github.com/Astrum-Project/AstralClone")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
